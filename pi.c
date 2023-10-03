73d9fd45ee5f638caa976204da8369944639d2db
Dhatvik.M.P <75828380+dhatvik@users.noreply.github.com>
2023-10-03 10:14:39 +0530

    Update pi.c

--- pi.c
+++ pi.c
@@ -28,3 +28,20 @@ sum[0] += local_sum;
 pi = sum[0]/num_steps;
 printf("pi = %6.12f\n",pi);
 }
+
+// sequential calculation of pi
+int num_steps=100000;
+double step;
+int main()
+{
+ int i;
+ double x, pi, sum=0.0;
+ step=1.0/(double)num_steps;
+ for(i=0;i<num_steps;i++)
+ {
+ x=(i+0.5)*step;
+ sum=sum+4.0/(1.0+x*x);
+ }
+ pi=step*sum;
+ printf("%6.12f ",pi);
+}
