4e6b61a873f95f7963d9540b52ed7e38e8923a90
Dhatvik.M.P <75828380+dhatvik@users.noreply.github.com>
2023-11-21 09:53:38 +0530

    Update 12.reduceall.c

--- PARTB/12.reduceall.c
+++ PARTB/12.reduceall.c
@@ -1,32 +1,35 @@
-#include <mpi.h>
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<mpi.h>
 
-int main() {
-    MPI_Init(NULL, NULL);
+int main(){
 
-    int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    int inbuf[2];
+    int outbuf[2];
+    MPI_Init(NULL,NULL);
+    int world_rank,world_size;
+    MPI_Comm_rank(MPI_COMM_WORLD,&world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD,&world_size);
+    int buf[2];
 
-    // Each process uses its rank as the data
-    int data = world_rank;
-    int max_val, min_val, sum, prod;
+    if(world_rank==0){
+        inbuf[0]=7;
+        inbuf[1]=1;
+    }
+    
+    else if(world_rank==1){
+        inbuf[0]=3;
+        inbuf[1]=2;
+    }
 
-    // Allreduce to find the maximum value
-    MPI_Allreduce(&data, &max_val, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
-
-    // Allreduce to find the minimum value
-    MPI_Allreduce(&data, &min_val, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
-
-    // Allreduce to find the sum of all values
-    MPI_Allreduce(&data, &sum, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
-
-    // Allreduce to find the product of all values
-    MPI_Allreduce(&data, &prod, 1, MPI_INT, MPI_PROD, MPI_COMM_WORLD);
-
-    // Each process prints the results
-    printf("Process %d - Max: %d, Min: %d, Sum: %d, Product: %d\n", 
-            world_rank, max_val, min_val, sum, prod);
-
+    if(world_rank==2){
+        inbuf[0]=5;
+        inbuf[1]=2;
+    }
+    
+    MPI_Allreduce(inbuf,outbuf,2,MPI_INT,MPI_PROD,MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD);
+    printf("Total sum is %d  %d",outbuf[0],outbuf[1]);
     MPI_Finalize();
     return 0;
-}
+}    
